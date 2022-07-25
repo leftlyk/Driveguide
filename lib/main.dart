a39aeb15898089b80c58a88484931852476de7dd
leftlyk <62270400+leftlyk@users.noreply.github.com>
2022-07-25 12:00:48 +1200

    PAges

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[800],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage(''),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.green[800],
+      ),
+    );
+  }
+}
+
